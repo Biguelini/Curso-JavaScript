6140c71fc00202c57ef31d3d2f5aea79301734b2
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-05-21 23:08:11 -0300

    update

--- react/crud/frontend/src/components/template/Footer.jsx
+++ react/crud/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import './Footer.css'
 import React from 'react'
 export default props => 
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Biguelini</strong>
+        </span>
     </footer>
