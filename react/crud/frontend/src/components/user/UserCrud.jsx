14b407b66710dd731d92e84409576b9019c81fa7
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-05-23 13:49:42 -0300

    update

--- react/crud/frontend/src/components/user/UserCrud.jsx
+++ react/crud/frontend/src/components/user/UserCrud.jsx
@@ -32,9 +32,9 @@ export default class UserCrud extends Component {
             this.setState({ user: initialState.user, list })
         })
     }
-    getUpdatedList(user) {
+    getUpdatedList(user, add=true) {
         const list = this.state.list.filter((u) => u.id !== user.id)
-        if(user) list.unshift(user)
+        if (add) list.unshift(user)
         return list
     }
     updateField(event) {
@@ -98,13 +98,56 @@ export default class UserCrud extends Component {
     load(user) {
         this.setState({ user })
     }
-    remove(user){
-        axios.delete(`${baseUrl}/${user.id}`).then(res=>{
-            const list = this.getUpdatedList(null)
-            this.setState(list)
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>{this.renderRows()}</tbody>
+            </table>
+        )
+    }
+    renderRows() {
+        return this.state.list.map((user) => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button
+                            className="btn btn-warning"
+                            onClick={() => this.load(user)}
+                        >
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button
+                            className="btn btn-danger ml-2 "
+                            onClick={() => this.remove(user)}
+                        >
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+    remove(user) {
+        axios.delete(`${baseUrl}/${user.id}`).then((res) => {
+            const list = this.getUpdatedList(user,false)
+            this.setState({ list })
         })
     }
     render() {
-        return <Main {...headerProps}>{this.renderForm()}</Main>
+        return (
+            <Main {...headerProps}>
+                {this.renderForm()}
+                {this.renderTable()}
+            </Main>
+        )
     }
 }
