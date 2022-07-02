a4308e672bf12fda8b22379d2c1b27da41907738
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-07-02 16:05:41 -0300

    separando projetos

--- react/crud/frontend/src/components/template/Footer.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import './Footer.css'
-import React from 'react'
-export default props => 
-    <footer className="footer">
-        <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Biguelini</strong>
-        </span>
-    </footer>
